887c42da1dd7ebaa5dbe38e7d4c7cc17ee9c2ac1
slametpolkam <slamet@poltek-kampar.ac.id>
2022-03-29 11:48:33 +0700

    Perubahan dari desktop

---
+++ src/latihan1b/Latihan1b.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package latihan1b;
+
+/**
+ *
+ * @author Slamet Triyanto
+ * Date 22/03/2022 10:25
+ */
+public class Latihan1b {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.print("Hallo Dunia");
+    }
+    
+}
